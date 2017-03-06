cfbdd4a43bafd3a79ccfa29a71e565616da7d15d
Allison Alexander <aveleber@hotmailcom>
2017-03-06 18:27:12 -0500

    end of day commute

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const volleyball = require('volleyball');
 const app = express();
+
+app.use(function (req, res, next) {
+	console.log(Object.keys(req).sort());
+	console.log('Request Type:', req.method, 'Request URL: ', req.originalUrl);
+
+	next();
+});
+
+app.use(volleyball);
+
+app.use('/special',function(req, res, next) {
+	console.log('You reached the special area', req.originalUrl);
+	next();
+});
 
 app.get('/', function(req, res){
 	res.send('<h1>Welcome to our server!!<\/h1>');
-})
+});
 
+app.get('/news', function(req, res) {
+	res.send('<h1>News Headlines<\/h1>');
+});
 
 var server = app.listen(3000, () => {
 	console.log('in app.listen');
