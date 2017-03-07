4c78fdcbbc3674ae07581572c0131a2567dd3e25
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-03-07 14:31:44 -0500

    Routing and Views #1

--- app.js
+++ app.js
@@ -11,12 +11,11 @@ app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
 
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(function (req, res, next) {
 //	console.log(Object.keys(req).sort());
 	console.log('Request Type:', req.method, 'Request URL: ', req.originalUrl);
-
 	next();
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const router = express.Router();
-// could use one line instead: const router = require('express').Router();
+
 const tweetBank = require('../tweetBank');
 
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
-  //res.sendFile('/public/stylesheets/style.css');
-  //  res.sendFile('/Users/allison/Immersive/twitter-js/public/stylesheets/style.css');
   	res.render( 'index', { tweets: tweets } );
 });
-
-// router.get('/stylesheets/style.css', function (req, res) {
-//   //let tweets = tweetBank.list();
-  	
-//     res.sendFile(styleSheet);
-//   //	res.render( 'index', { tweets: tweets } );
-// });
 
 
 
