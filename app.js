57cb16cd591ec835827f1360c8b65506628f7c12
Allison Alexander <aveleber@hotmailcom>
2017-03-07 12:25:03 -0500

    static routing

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const volleyball = require('volleyball');
 const nunjucks = require('nunjucks');
+const routes = require('./routes');
+const path = require('path');
 const app = express();
+
+app.use('/', routes);
+
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
 
+app.use(express.static(path.join(__dirname, '/public')))
+
 app.use(function (req, res, next) {
-	console.log(Object.keys(req).sort());
+//	console.log(Object.keys(req).sort());
 	console.log('Request Type:', req.method, 'Request URL: ', req.originalUrl);
 
 	next();
@@ -28,24 +35,24 @@ app.use(volleyball);
 //     console.log(output);
 // });
 
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
-app.get('/views', function(req, res) {
-	res.render( 'index', {title: 'Hall of Fame', people: people} );
-});
+// app.get('/views', function(req, res) {
+// 	res.render( 'index', {title: 'Hall of Fame', people: people} );
+// });
 
-app.use('/special',function(req, res, next) {
-	console.log('You reached the special area', req.originalUrl);
-	next();
-});
+// app.use('/special',function(req, res, next) {
+// 	console.log('You reached the special area', req.originalUrl);
+// 	next();
+// });
 
 app.get('/', function(req, res){
 	res.send('<h1>Welcome to our server!!<\/h1>');
 });
 
-app.get('/news', function(req, res) {
-	res.send('<h1>News Headlines<\/h1>');
-});
+// app.get('/news', function(req, res) {
+// 	res.send('<h1>News Headlines<\/h1>');
+// });
 
 var server = app.listen(3000, () => {
 	console.log('in app.listen');
